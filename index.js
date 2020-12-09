16cd87da93a41162bc5c82648eff9d4a3b4c6b9d
speedsul <spedsul@gmail.com>
2020-12-09 15:47:38 -0300

    add exercicio 2 and 3

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ function init() {
     try {
       const dataEstado = JSON.parse(await fs.readFile(estado, 'utf-8'));
       const dataCidade = JSON.parse(await fs.readFile(cidade, 'utf-8'));
-
+      console.log('1. Criar uma função que irá criar um arquivo JSON');
       dataEstado.map((estado) => {
         const data = filterCity(dataCidade, estado);
         const { Sigla } = estado;
-        criaJson(Sigla, data);
+        criaJson(Sigla, data).then(() =>
+          console.log(
+            'Arquivos criados com sucesso na pasta:',
+            `./dataCity/${estado.Sigla}.json`
+          )
+        );
       });
     } catch (erro) {
       console.log(erro);
@@ -32,22 +37,48 @@ function init() {
 
   const getCityOfState = async (state) => {
     const result = await readJson(state);
-    const cidadesPorEstado = `Cidades por estado: ${state} = ${result.length}`;
-    // console.log(cidadesPorEstado);
-    return result.length;
+    const quantCity = result.length;
+
+    // console.log(obj);
+    array.push({ Estado: [state], Quantidade: quantCity });
+    return array;
   };
+  // Parte 02
 
   const cicoEstadosMaisCidades = async () => {
-    const dataEstado = JSON.parse(await fs.readFile(estado, 'utf-8'));
-    const estadoOrder = [];
-    const dat = dataEstado.map((estado) => {
-      getCityOfState(estado.Sigla).then((resp) => console.log([resp]));
-
-      // return estadoOrder;
-    });
+    try {
+      const dataEstado = JSON.parse(await fs.readFile(estado, 'utf-8'));
+      async function pegaCidade() {
+        dataEstado.map((estado) => {
+          getCityOfState(estado.Sigla);
+        });
+        // console.log(array.sort((a, b) => a[1] - b[1]));
+        console.log(array);
+      }
+      await pegaCidade();
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   estados();
-  getCityOfState('SP'); //quantidades de cidades do estado
+  //quantidades de cidades do estado
+
+  // getCityOfState('SP');
   cicoEstadosMaisCidades();
 }
+
+const array = [];
+
+setTimeout(() => {
+  const quantidade = array.length;
+
+  let teste = array.sort((a, b) => b.Quantidade - a.Quantidade).slice(0, 5);
+  console.log('\nOs estados que mais Possuem cidades no Brasil:');
+  console.log(teste);
+  let cincoMenores = array
+    .sort((a, b) => b.Quantidade - a.Quantidade)
+    .slice(22, quantidade);
+  console.log('\nOs estados que menos Possuem cidades no Brasil:');
+  console.log(cincoMenores);
+}, 2000);
